0f5fa83f34f0ed84a890ef6f411c8ee278a08eff
sectumsemper <kaciy11@yahoo.co.uk>
2019-11-27 02:48:10 +0100

    added logging & groundwork for sessions

--- GAGH/run.py
+++ GAGH/run.py
@@ -1,8 +1,12 @@
-from flask import Flask,g,render_template,request
+from flask import Flask,g,render_template,request, url_for,session
+from logging.handlers import RotatingFileHandler
+
 import sys
+import os
 import sqlite3 as sql,configparser
 
 app=Flask(__name__)
+app.secret_key=os.urandom(24)
 
 db_location='var/GAGH.db'
 html_location='static/html/'
@@ -27,47 +31,7 @@ def root():
 @app.route("/submit/")
 def new_review():
    return render_template('submit.html')
-
-@app.route('/test/')
-def test():
-    db=get_db();
-    msg='empty msg'
-    try:
-        reviewer_id = 'ksdjhf'
-        barbershop_id = 'lsdkfjlksdjf'
-        date_visited = 58937
-        date_added = 2340598
-        title = 'aaaaaa'
-        review_text = 'sdfgsdfgsdfgdsfgsdfg'
-        haircut_rating = 5
-        anxiety_rating = 3
-        friendliness_rating = 1
-        pricerange = 2
-     
-        gender_remarks = 1
-        gender_charged = 1
-        unsafe = 0
-
-        barber_id=None
-        barber_recommended=0
-
-        db.cursor().execute("INSERT INTO Review (reviewer_id,barbershop_id,date_visited,date_added,title,review_text,haircut_rating,anxiety_rating,friendliness_rating,pricerange,gender_remarks,gender_charged,unsafe,barber_id,barber_recommended) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",(reviewer_id,barbershop_id,date_visited,date_added,title,review_text,haircut_rating,anxiety_rating,friendliness_rating,pricerange,gender_remarks,gender_charged,unsafe,barber_id,barber_recommended) )
-        db.commit()
-        msg = "Record successfully added"
-    except sql.Error as error:
-        db.rollback()
-        msg = "Error in insert operation: "+str(error)
-    finally:
-        page = []
-        page.append('<html><ul>')
-    # sql = "SELECT * FROM Review ORDER BY barbershop_id"
-    # for row in db.cursor().execute(sql):
-    #     page.append('<li>')
-    #     page.append(str(row))
-    #     page.append('</li>')
-        page.append(msg)
-        page.append('</ul><html>')
-        return ''.join(page)
+    
 
 @app.route('/submit/submit-review',methods = ['POST', 'GET'])
 def submit_review():
@@ -130,6 +94,7 @@ def config():
     strg.append('ip_address:'+app.config['ip_address'])
     return '\t'.join(str)
 
+
 def init(app):
     config = configparser.ConfigParser()
     try:
@@ -141,14 +106,31 @@ def init(app):
         app.config['port'] = config.get("config", "port")
         app.config['url'] = config.get("config", "url")
 
+        app.config['log_file'] = config.get("logging", "name")
+        app.config['log_location'] = config.get("logging", "location")
+        app.config['log_level'] = config.get("logging", "level")
+
     except:
         print ("Could not read configs from: ", config_location)
 
+
+def logs(app):
+    log_pathname = app.config['log_location'] + app.config['log_file']
+    file_handler = RotatingFileHandler(log_pathname, maxBytes=1024* 1024 * 10 , backupCount=1024)
+    file_handler.setLevel( app.config['log_level'] )
+    formatter = logging.Formatter("%(levelname)s | %(asctime)s |  %(module)s | %(funcName)s | %(message)s")
+    file_handler.setFormatter(formatter)
+    app.logger.setLevel( app.config['log_level'] )
+    app.logger.addHandler(file_handler)
+
 init(app)
+logs(app)
 
 
 if __name__ == "__main__":
     init(app)
+    logs(app)
+
     app.run(
         host=app.config['ip_address'],
         port=int(app.config['port']))
