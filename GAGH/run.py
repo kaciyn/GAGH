7c12c6917ab64d3ead1e98165e5ade2edd1dd36b
sectumsemper <kaciy11@yahoo.co.uk>
2019-12-02 23:08:14 +0100

    fixed the hashpw calls but. Have I

--- GAGH/run.py
+++ GAGH/run.py
@@ -152,7 +152,7 @@ def check_auth(email, password):
     if(result is None):
         app.logger.error('User '+email+' not found')
         return False
-    elif (result.hash_password == bcrypt.hashpw(password.encode('utf-8'), result.hash_password)):
+    elif (result['hash_password'] == bcrypt.hashpw(password.encode('utf-8'), result['hash_password'])):
         app.logger.info('Correct password for user '+email)
         return True
     else:
@@ -162,7 +162,7 @@ def check_auth(email, password):
 def get_user(email):
     try:
         result=query_db("SELECT email,hash_password FROM User WHERE email = ?",[email],one=True)
-        app.logger.info('Successfully retrieved user '+result[0])
+        app.logger.info('Successfully retrieved user '+result['email'])
         app.logger.info('Result length: '+result.len())
     
     except sql.Error as error:
