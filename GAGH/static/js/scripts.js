90cdbb6514565937adae96d6f3680801961cac36
sectumsemper <kaciy11@yahoo.co.uk>
2019-12-04 04:04:29 +0100

    HOKAY lol so i've put together a query for the barbershops+reviews & such to spit into the barbershops page can't wait for it to Not Work :-)

--- GAGH/static/js/scripts.js
+++ GAGH/static/js/scripts.js
@@ -1,9 +1,1 @@
-﻿<script>
-    $(function(){
-        $('.datepicker').datepicker({
-            format: 'mm-dd-yyyy',
-            endDate: '+0d',
-            autoclose: true
-        });
-    });
-</script>
+﻿
