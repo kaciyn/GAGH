dac3cd1ee64a2d8e5f8ec103ff02eb8d40152fe4
sectumsemper <kaciy11@yahoo.co.uk>
2020-01-05 01:14:29 +0100

    Early groundwork for actual error handling/bad response routing

---
+++ GAGH/GAGH/handlers/errors.py
@@ -0,0 +1,10 @@
+# from flask import Blueprint
+#
+#
+# bp = Blueprint('errors', __name__)
+#
+#
+# @bp.errorhandler(404)
+# def notfound():
+#     """Serve 404 template."""
+#     return make_response(render_template("404.html"), 404)
